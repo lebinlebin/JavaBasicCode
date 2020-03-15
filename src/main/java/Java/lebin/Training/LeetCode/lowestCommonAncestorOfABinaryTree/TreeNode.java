340ada1d48d856de4fc10e72d47f8fdb8c2cce9e
liulebin <liulebin@58.com>
2020-03-15 12:26:25 +0800

    java 机器学习系统及java基础复习

---
+++ src/main/java/Java/lebin/Training/LeetCode/lowestCommonAncestorOfABinaryTree/TreeNode.java
@@ -0,0 +1,8 @@
+package Java.lebin.Training.LeetCode.lowestCommonAncestorOfABinaryTree;;
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
---
+++ src/main/java/Java/lebin/Training/LeetCode/maximumDepthOfBinaryTree/TreeNode.java
@@ -0,0 +1,8 @@
+package Java.lebin.Training.LeetCode.maximumDepthOfBinaryTree;;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
